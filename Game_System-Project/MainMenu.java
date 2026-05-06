b52aa5522689d25983211eeba370e7862f14dc2d
MaxDom5022 <mudomi200@rbc.edu>
2026-05-06 00:28:55 -0400

    Saving point

---
+++ Game_System-Project/MainMenu.java
@@ -0,0 +1,17 @@
+// This is the MainMenu main class is for game.java
+
+//importing scanner
+import java.util.Scanner;
+
+public class MainMenu {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int choice = 0;
+
+        //Games (Objects)
+
+    }
+
+}
+
+
