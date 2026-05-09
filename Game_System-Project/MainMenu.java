949d8087ae76e52dba35d18d5a3ac5b500b81ee2
MaxDom5022 <mudomi200@rbc.edu>
2026-05-08 21:53:30 -0400

    Main menu Complete

--- Game_System-Project/MainMenu.java
+++ Game_System-Project/MainMenu.java
@@ -1,6 +1,7 @@
 // This is the MainMenu main class is for game.java
 
 //importing scanner
+
 import java.util.Scanner;
 
 public class MainMenu {
@@ -9,6 +10,37 @@ public class MainMenu {
         int choice = 0;
 
         //Games (Objects)
+        guessTheNumber guessGame = new guessTheNumber();
+        rockPaperScissors rpsGame = new rockPaperScissors();
+
+        //while loop
+        while (choice != 3) {
+            System.out.println("\n Welcome to the RBC Game Hub");
+            System.out.println("----------------------------");
+            System.out.println("Please Select a game you would like to play:");
+            System.out.println("1. Guess the Number");
+            System.out.println("2. Rock, Paper, Scissors");
+            System.out.println("3. Quit");
+
+            choice = input.nextInt();
+
+            //if statements
+            if (choice == 1){
+                guessGame.start();
+                guessGame.play();
+
+            }else if (choice == 2){
+                rpsGame.start();
+                rpsGame.play();
+
+            }else if (choice == 3){
+                System.out.println("Goodbye!!!");
+
+            }else System.out.println("Invalid Choice, please enter 1. 2. 3.");
+
+
+
+        }
 
     }
 
